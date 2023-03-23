3a4a632fae35d8d1deb5e8584b26d945782bdf55
Cesar Herrera <cahf2001@gmail.com>
2023-03-22 23:33:44 -0600

    terminado 22-03-22

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
-import { Card } from "../components/cards";
+import {Card} from "../components/cards"
+
 export const Home:FC = () =>{
     return(
         <>
@@ -7,6 +8,22 @@ export const Home:FC = () =>{
         <h1>Home</h1>
         <Card>
             <h2>This is a children Node</h2>
+            <p>2021-03-21</p>
+        </Card>
+        <Card>
+            <h2>This is a children Node1</h2>
+            <p>2021-03-21</p>
+        </Card>
+        <Card>
+            <h2>This is a children Node2</h2>
+            <p>2021-03-21</p>
+        </Card>
+        <Card>
+            <h2>This is a children Node3</h2>
+            <p>2021-03-21</p>
+        </Card>
+        <Card>
+            <h2>This is a children Node4</h2>
             <p>2021-03-21</p>
         </Card>
         </>
